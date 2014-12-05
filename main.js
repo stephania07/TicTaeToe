3bac1410103f958b95e8fc543b765a9631c61270
Stephania <stephania07>
2014-12-05 08:18:54 -0600

    Added some move functionality

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
 ;(function(){
-//global variables
-var currentpiece;
+  'use strict';
 
-//make DOM ready
-$(document).ready(function(){
-  
+// global variables
+var count = 0;
+var turn ='player1';
+var board = [['', '', ''],['', '', ''],['', '', '']];
+
+$('td').click(function(){
+  if(!($(this).hasClass('moveX')) && !($(this).hasClass('moveO'))) {
+    playerTurn($(this)); 
+    boardStatus();
+    updatedBoard(board);
+  }
 });
 
-$('li').draggable({
-  snap: 'td',
-  containment: 'table',
-	strat: function(event, ui){
-	currentpiece = $(this);
-	}			
-});
+function playerTurn(activeCell){
+    if(turn === 'player1') {
+       $(activeCell).addClass('moveX');
+       turn = 'player2';
+    }else if(turn === 'player2'){
+       $(activeCell).addClass('moveO');
+       turn = 'player1';
+    } 
+}
 
+function boardStatus(){
+	var newBoard = [];
+	$('.grid tr').each(function(){
+		var $row = $(this);
+		var newRow = [];
+	    $row.find('td').each(function(){
+          var $cell = $(this);
+          if($cell.hasClass('moveX')){
+            newRow.push('X');  
+          }else if($cell.hasClass('moveO')){
+            newRow.push('O');
+          }else{
+            newRow.push('');
+          }
+		});
+		newBoard.push(newRow);
+	});
+	board = newBoard;
+}
+
+function updatedBoard(updatedBoard){
+//looping thru 
+  $('.grid tr').each(function(){
+	  var $newRow = $(this);
+		  $newRow.find('td').each(function(){
+			  var $cell = $(this);
+				if($cell === 'X' || $cell === 'O'){
+				 // $(updatedBoard)
+				}
+
+			});
+	});
+} 
 })();
+
+
+	
+
